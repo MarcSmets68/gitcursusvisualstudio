03aaa3df408a02058be72f457d32b3e6fe39be1e
MarcSmets68 <marc.smets68@gmail.com>
2018-03-15 11:33:10 +0100

    hallo allemaal

--- GitCursus/Program.cs
+++ GitCursus/Program.cs
@@ -10,7 +10,7 @@ namespace GitCursus
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Iedereen");
+            Console.WriteLine("Hallo allemaal");
         }
     }
 }
